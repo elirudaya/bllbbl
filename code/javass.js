014e2126b20e9c634d2e5bd7068f183b533a4bd8
elirudaya <rudayaelinaa@gmail.com>
2025-01-27 15:40:26 +0300

    m

---
+++ code/javass.js
@@ -0,0 +1,28 @@
+document.addEventListener ('DOMContentLoaded'), function(){
+
+// задача 1
+let age = parseInt(prompt('введите ваш возраст:'));
+if (age >= 60) {
+    console.log ('Поздравляем с пенсионным возрастом!');}
+else {
+    console.log ('Вам ещё рано на пенсию');}
+
+    
+// задача 2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+}
